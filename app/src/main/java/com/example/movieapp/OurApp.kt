f89bb664dd750929f9c4c4b574d975f5d719a9be
Sardor <sardor.islomov.96@gmail.com>
2021-12-31 17:22:26 +0500

    Commits added

--- app/src/main/java/com/example/movieapp/OurApp.kt
+++ app/src/main/java/com/example/movieapp/OurApp.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp
 
 import android.app.Application
+import android.util.Log
 import com.example.movieapp.koin.applicationComponent
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -10,6 +11,7 @@ class OurApp: Application() {
     override fun onCreate() {
         super.onCreate()
         setUpKoin()
+        Log.e("Islomov","")
     }
 
     private fun setUpKoin() {
